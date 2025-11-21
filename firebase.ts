f0a18086fff2d208b199c3e651388a7008844cc3
imdevanshm <imdevanshm@gmail.com>
2025-11-21 11:55:23 +0000

    feat: Merge stashed changes and resolve conflicts after pull

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "royal-wedding-portal.firebaseapp.com",
-  projectId: "royal-wedding-portal",
-  storageBucket: "royal-wedding-portal.appspot.com",
-  messagingSenderId: "xxxxxxxxxxxxx",
-  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  measurementId: "xxxxxxxxxxxxx"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
